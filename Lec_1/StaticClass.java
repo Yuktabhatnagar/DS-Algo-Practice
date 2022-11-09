a1356cdd9bc51c5ed81ff1bf0ecb3514a2b17ef8
Yukta Bhatnagar <74857997+Yuktabhatnagar@users.noreply.github.com>
2022-11-09 10:47:39 +0530

    Add files via upload

---
+++ Lec_1/StaticClass.java
@@ -0,0 +1,13 @@
+package Lec_1;
+
+public class StaticClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int  a= little;
+//		int b=10;
+//		b="Kunal";
+		System.out.println(a+120);
+	}
+
+}
